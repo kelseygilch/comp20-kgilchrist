dda9d5f6b4ae494da5ca665fee55226877fd7bed
Kelsey Gilchrist <kgilchrist15@gmail.com>
2015-02-26 16:33:11 -0500

    added ms pacman

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,31 +1,23 @@
-// Replace this with your code...
-
 
 //<body onload="msPacMan">
 
-function init{
+function init(){
 
-var canvas = document.getElementById('game_canvas');
+var canvas = document.getElementById("game_canvas");
 var ctx = canvas.getContext("2d");
-var google = new Image();
+var img = new Image();
 
 img.addEventListener("load", function() {
 
-ctx.drawImage(google, 315, 0, 464, 134, 10, 10, 800, 600);
+ctx.drawImage(img, 315, 0, 464, 134, 0, 0, 464, 134);
+ctx.drawImage(img, 80, 0, 17, 17, 120, 90, 17, 17);
+
 
 }, false);
 
-google.src = 'pacman10-hp-sprite.png';
+img.src = "pacman10-hp-sprite.png";
 
 
 }
 
-
-//msPacMan {
-
-
-
-
-
-//}
 
