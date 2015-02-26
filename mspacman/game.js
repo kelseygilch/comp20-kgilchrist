0736573dd4df0805e84cdb9c512b2099d0b7c3cd
Kelsey Gilchrist <kgilchrist15@gmail.com>
2015-02-26 16:21:09 -0500

    canvas var

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,31 @@
 // Replace this with your code...
+
+
+//<body onload="msPacMan">
+
+function init{
+
+var canvas = document.getElementById('game_canvas');
+var ctx = canvas.getContext("2d");
+var google = new Image();
+
+img.addEventListener("load", function() {
+
+ctx.drawImage(google, 315, 0, 464, 134, 10, 10, 800, 600);
+
+}, false);
+
+google.src = 'pacman10-hp-sprite.png';
+
+
+}
+
+
+//msPacMan {
+
+
+
+
+
+//}
+
